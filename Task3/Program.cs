ffd284dc8513665e26a4b17fa5a5f6ea7bfca316
Sinice1981 <vinakubani@rambler.ru>
2022-09-24 20:27:17 +0300

    Added 3 task

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N > 0),
+// а на выходе показывает все чётные числа от 1 до N
+Console.WriteLine("Введите число больше 0");
+int num = Convert.ToInt32(Console.ReadLine());
+int min = 0;
+
+while (min <=num-2)
+{
+    min=min+2;
+Console.Write(min + ",");
+} 
